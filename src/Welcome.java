2ef14f21f23a32dfbba08b5b590c874f8ffbc809
dilip-sdet <dilipraj611@gmail.com>
2025-12-16 04:42:40 +0530

    Commit:Updated Welcome.Java appending "Learning"

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,5 +1,5 @@
 public class Welcome {
     public static void main(String[] args){
-        System.out.println("Welcome Program");
+        System.out.println("Welcome Program Learning");
     }
 }
