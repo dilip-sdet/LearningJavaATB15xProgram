7bde1c684888364920386a5eaeab051f155bab48
dilip-sdet <dilipraj611@gmail.com>
2025-12-25 22:48:52 +0530

    Commit:Updated Welcome.Java appending "Learning"

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -1,4 +1,6 @@
-public class Welcome {
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
     public static void main(String[] args){
         System.out.println("Welcome Program Learning");
     }
