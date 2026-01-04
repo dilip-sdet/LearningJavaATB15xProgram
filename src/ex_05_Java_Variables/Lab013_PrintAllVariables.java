89933251efa7bf0ce1ff16747350a9ad423b191b
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 19:42:16 +0530

    Commit:I have written Variable Program

---
+++ src/ex_05_Java_Variables/Lab013_PrintAllVariables.java
@@ -0,0 +1,22 @@
+package ex_05_Java_Variables;
+
+public class Lab013_PrintAllVariables {
+    public static void main(String[] args) {
+        boolean bool = true;
+        byte a = 4;
+        short b = 5;
+        int c = 6;
+        long d = 10L;
+        float f = 10.1f;
+        double deci = 10.12345566;
+        char ch = 'A';
+        System.out.println(bool);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(deci);
+        System.out.println(ch);
+    }
+}
