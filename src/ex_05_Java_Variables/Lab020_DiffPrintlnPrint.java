226d508d62c7313e0f27e88256b93e798ac0530d
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 20:51:12 +0530

    Commit:I have written Println and Print Program

---
+++ src/ex_05_Java_Variables/Lab020_DiffPrintlnPrint.java
@@ -0,0 +1,10 @@
+package ex_05_Java_Variables;
+
+public class Lab020_DiffPrintlnPrint {
+    public static void main(String[] args) {
+        System.out.println("Println creates a new line");
+        System.out.print("Print creates a same line1");
+        System.out.print("Print creates a new line2");
+
+    }
+}
