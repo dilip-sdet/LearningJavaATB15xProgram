40f054df24c6f6c306df382339611259b45d13db
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 19:32:39 +0530

    Commit:I have written Variable Program

---
+++ src/ex_05_Java_Variables/Lab011_Variable.java
@@ -0,0 +1,12 @@
+package ex_05_Java_Variables;
+
+public class Lab011_Variable {
+    public static void main(String[] args) {
+        byte age_Dilip = 32;
+        //byte -> datatypes
+        //age_Dilip -> variables or Identifiers
+        //= -> Assignment Operator
+        //32 -> Literals or Value
+        System.out.println(age_Dilip);
+    }
+}
