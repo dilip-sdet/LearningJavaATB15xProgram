cd13fcef99372652f852516df2a02f6060dc6f0b
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 19:36:29 +0530

    Commit:I have written Variable Program

---
+++ src/ex_05_Java_Variables/Lab012_VariableReinitialization.java
@@ -0,0 +1,9 @@
+package ex_05_Java_Variables;
+
+public class Lab012_VariableReinitialization {
+    public static void main(String[] args) {
+        byte age_Dilip = 32;
+        age_Dilip = 100;
+        System.out.println(age_Dilip);
+    }
+}
