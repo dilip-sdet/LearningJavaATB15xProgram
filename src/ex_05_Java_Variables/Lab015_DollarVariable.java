b46bcdaa1dc50d5e9096ea35ab60c34b51932a26
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 19:53:23 +0530

    Commit:I have written Variable Program

---
+++ src/ex_05_Java_Variables/Lab015_DollarVariable.java
@@ -0,0 +1,9 @@
+package ex_05_Java_Variables;
+
+public class Lab015_DollarVariable {
+    public static void main(String[] args) {
+        int $ = 13;
+        System.out.println($);
+        // " _" will npt work in java 25
+    }
+}
