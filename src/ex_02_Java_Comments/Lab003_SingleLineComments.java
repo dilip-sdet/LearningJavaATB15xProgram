2beb10fced6a90b449e725d8b3fab824bf04d582
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 15:56:53 +0530

    Commit:I have added comments Program

---
+++ src/ex_02_Java_Comments/Lab003_SingleLineComments.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Comments;
+
+public class Lab003_SingleLineComments {
+    public static void main(String[] args) {
+        //This is a comments
+        //Also this is a Single line Comment
+        //This will not execute the program
+        System.out.println("Welcome to Java");
+    }
+}
---
+++ src/ex_02_Java_Comments/Lab004_MultiLineComments.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Comments;
+
+public class Lab004_MultiLineComments {
+    public static void main(String[] args) {
+
+        /*
+        Author: Dilip Kumar K
+        Course: Java Selenium Automation
+        Lucturer : Pramod Dutta
+         */
+        System.out.println("Welcome to Java");
+    }
+}
