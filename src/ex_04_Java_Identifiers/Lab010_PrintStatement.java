9aa8fc627e50c8f159b1e2cae6ba88d0e6c2db8f
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 17:05:07 +0530

    Commit:I have written Print Program

---
+++ src/ex_04_Java_Identifiers/Lab010_PrintStatement.java
@@ -0,0 +1,9 @@
+package ex_04_Java_Identifiers;
+
+public class Lab010_PrintStatement {
+    public static void main(String[] args) {
+        System.out.print("Dilip");
+        System.out.print("Kumar");
+        System.out.print("K");
+    }
+}
