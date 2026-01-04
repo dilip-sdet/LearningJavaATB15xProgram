63a32d431667ab10c0873052f28717306003b329
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 20:05:44 +0530

    Commit:I have written Variable Program

---
+++ src/ex_05_Java_Variables/Lab017_EnumVariable.java
@@ -0,0 +1,9 @@
+package ex_05_Java_Variables;
+
+public class Lab017_EnumVariable {
+    public static void main(String[] args) {
+        int Enum = 23;
+        System.out.println(Enum);
+        //enum is a keyword, cannot be used as a variable
+    }
+}
