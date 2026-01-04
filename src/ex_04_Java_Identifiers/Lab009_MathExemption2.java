70ea81086267c94b18924388d5e4c8c27b6e4686
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 16:50:24 +0530

    Commit:I have written Math Exemption Program

--- src/ex_04_Java_Identifiers/Lab009_MathExemption2.java
+++ src/ex_04_Java_Identifiers/Lab009_MathExemption2.java
@@ -1,6 +1,6 @@
 package ex_04_Java_Identifiers;
 
-public class Lab_009_MathExemption2 {
+public class Lab009_MathExemption2 {
     public static void main(String[] args) {
         System.out.println(1/0);
     }
