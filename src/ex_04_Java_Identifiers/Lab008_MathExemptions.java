aefe661c073fbc4edb77106b57c0dbd8184a8784
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 16:47:07 +0530

    Commit:I have written Math Exemption Program

---
+++ src/ex_04_Java_Identifiers/Lab008_MathExemptions.java
@@ -0,0 +1,8 @@
+package ex_04_Java_Identifiers;
+
+public class Lab008_MathExemptions {
+    public static void main(String[] args) {
+        System.out.println("2+2");
+        System.out.println(2+2);
+    }
+}
