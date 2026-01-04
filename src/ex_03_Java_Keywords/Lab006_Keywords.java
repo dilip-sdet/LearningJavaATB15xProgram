af8a63634ede6ce500a9f3c28593cd80ab756032
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 16:17:46 +0530

    Commit:I have written Keyword related program

---
+++ src/ex_03_Java_Keywords/Lab006_Keywords.java
@@ -0,0 +1,9 @@
+package ex_03_Java_Keywords;
+
+public class Lab006_Keywords {
+    public static void main(String[] args) {
+        System.out.println("Welcome Program Learning");
+        // Here content which light sand color, is a Keyword
+        System.out.println("namely: package, public, class, static, void are Keywords");
+    }
+}
