175f24ebade25dc3ac82f5e52f81d472a86f3bc5
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 16:28:37 +0530

    Commit:I have written Identifier related program

---
+++ src/ex_04_Java_Identifiers/Lab007_Identifiers.java
@@ -0,0 +1,9 @@
+package ex_04_Java_Identifiers;
+
+public class Lab007_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("Welcome Program Learning");
+        // Here content which light sand color, is a Identifiers
+        System.out.println("namely: Lab007_Identifiers,ex_04_Java_Identifiers are Identifiers given to package and class respectively");
+    }
+}
