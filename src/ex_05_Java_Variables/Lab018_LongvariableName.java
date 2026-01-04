20d7c6284925e3c3db38862a0f682c9064a04668
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 20:09:33 +0530

    Commit:I have written Variable Program

---
+++ src/ex_05_Java_Variables/Lab018_LongvariableName.java
@@ -0,0 +1,8 @@
+package ex_05_Java_Variables;
+
+public class Lab018_LongvariableName {
+    public static void main(String[] args) {
+        byte my_age_till_2025_yaer_is = 32;
+        System.out.println(my_age_till_2025_yaer_is);
+    }
+}
