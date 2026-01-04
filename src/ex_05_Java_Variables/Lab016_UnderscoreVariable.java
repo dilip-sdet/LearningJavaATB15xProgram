e52916a5c05e56773e2ee1c90d3df324526cf4a9
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 19:56:48 +0530

    Commit:I have written Variable Program

---
+++ src/ex_05_Java_Variables/Lab016_UnderscoreVariable.java
@@ -0,0 +1,8 @@
+package ex_05_Java_Variables;
+
+public class Lab016_UnderscoreVariable {
+    public static void main(String[] args) {
+        int _123 = 123;
+        System.out.println(_123);
+    }
+}
