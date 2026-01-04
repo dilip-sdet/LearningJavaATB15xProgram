84ce7fa2471d40a71d7aaf49e7ab2203580b0375
dilip-sdet <dilipraj611@gmail.com>
2026-01-04 16:02:20 +0530

    Commit:I have written both comments in single Program

---
+++ src/ex_02_Java_Comments/Lab005_BothComments.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Comments;
+
+public class Lab005_BothComments {
+    public static void main(String[] args) {
+        //This is a comments
+        //Also this is a Single line Comment
+        //This will not execute the program
+
+        /*
+        Author: Dilip Kumar K
+        Course: Java Selenium Automation
+        Lucturer : Pramod Dutta
+         */
+        System.out.println("Welcome to Java");
+    }
+}
