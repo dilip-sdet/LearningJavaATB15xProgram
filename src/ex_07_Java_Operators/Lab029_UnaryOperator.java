ec365ae5725daa2749f249dc71bfb1e6e22a222b
dilip-sdet <dilipraj611@gmail.com>
2026-01-05 13:38:52 +0530

    Commit:I have written Arithmatic program

---
+++ src/ex_07_Java_Operators/Lab029_UnaryOperator.java
@@ -0,0 +1,9 @@
+package ex_07_Java_Operators;
+
+public class Lab029_UnaryOperator {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = -400; //unary operator
+        System.out.println(a + b);
+    }
+}
