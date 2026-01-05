744fd73e6fc4e25c24470ccff1c5f8218c3598bf
dilip-sdet <dilipraj611@gmail.com>
2026-01-05 13:33:53 +0530

    Commit:I have modified lab13 program

---
+++ src/ex_07_Java_Operators/Lab028_ArithmaticOperatorsList.java
@@ -0,0 +1,23 @@
+package ex_07_Java_Operators;
+
+public class Lab028_ArithmaticOperatorsList {
+    public static void main(String[] args) {
+        /*
+        List of Arithmatic Operator:
+        -> "+" Addition
+        -> "-" Substraction
+        -> "*" Multiplication
+        -> "/" Division
+        -> "%" Modulus
+         */
+
+        int a = 41;
+        int b = 20;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
+}
