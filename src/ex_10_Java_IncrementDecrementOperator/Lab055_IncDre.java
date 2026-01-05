99bef0af366666bc9fef71f69d681be99bddea95
dilip-sdet <dilipraj611@gmail.com>
2026-01-05 18:04:39 +0530

    I have written Increment and Drecrement program

---
+++ src/ex_10_Java_IncrementDecrementOperator/Lab055_IncDre.java
@@ -0,0 +1,15 @@
+package ex_10_Java_IncrementDecrementOperator;
+
+public class Lab055_IncDre {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+
+        // Line 5 | a=10 | NA
+        // Line 6 | a=11 | b=11
+        // Line 7 | a=11
+        // Line 8 | b=11
+    }
+}
---
+++ src/ex_10_Java_IncrementDecrementOperator/Lab057_IncDre2.java
@@ -0,0 +1,10 @@
+package ex_10_Java_IncrementDecrementOperator;
+
+public class Lab057_IncDre2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = --a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
