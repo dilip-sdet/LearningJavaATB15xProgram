7000024fc4eafc7f2e7c3910fe62f07e9e1e4108
dilip-sdet <dilipraj611@gmail.com>
2026-01-05 14:18:46 +0530

    Commit:I have written concat interview program

---
+++ src/ex_07_Java_Operators/Lab032_ConcatInterview.java
@@ -0,0 +1,15 @@
+package ex_07_Java_Operators;
+
+public class Lab032_ConcatInterview {
+    public static void main(String[] args) {
+        String firstname = "Dilip ";
+        String lastname = "Kumar";
+        System.out.println(firstname + lastname);
+        //System.out.println(firstname - lastname); Not possible
+        //System.out.println(firstname / lastname); Not possible
+        //System.out.println(firstname * lastname); Not possible
+
+        // "+" act as a addition in case of integral data type
+        // "+" act a concat in String
+    }
+}
