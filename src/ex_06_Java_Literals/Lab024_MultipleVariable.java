13e82309de083ccb2906462a706195a1dc02267f
dilip-sdet <dilipraj611@gmail.com>
2026-01-05 12:45:23 +0530

    Commit:I have written Constant variable Program

---
+++ src/ex_06_Java_Literals/Lab024_MultipleVariable.java
@@ -0,0 +1,14 @@
+package ex_06_Java_Literals;
+
+public class Lab024_MultipleVariable {
+    public static void main(String[] args) {
+        int a = 10, b = 20, C = 30;
+        int d,e;
+        d = e = 40;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(C);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
