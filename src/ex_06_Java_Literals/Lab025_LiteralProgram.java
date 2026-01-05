f235cc4a6989488104f2f35bb3d662a5eede6aaf
dilip-sdet <dilipraj611@gmail.com>
2026-01-05 12:52:49 +0530

    Commit:I have written literal Program

---
+++ src/ex_06_Java_Literals/Lab025_LiteralProgram.java
@@ -0,0 +1,9 @@
+package ex_06_Java_Literals;
+
+public class Lab025_LiteralProgram {
+    public static void main(String[] args) {
+        int a = 10;
+        //datatype(int) variable name(a) (Operator)= Literal(10)
+        System.out.println(a);
+    }
+}
