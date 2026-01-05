1199b330a90280d4142aae6dd0b8b9ea04ac47fa
dilip-sdet <dilipraj611@gmail.com>
2026-01-05 14:32:20 +0530

    Commit:I have written interview program

---
+++ src/ex_07_Java_Operators/Lab034_Bodmass.java
@@ -0,0 +1,7 @@
+package ex_07_Java_Operators;
+
+public class Lab034_Bodmass {
+    public static void main(String[] args) {
+        System.out.println((9*3/9+1)*3);
+    }
+}
---
+++ src/ex_07_Java_Operators/Lab035_DoubleEqualtoInterview.java
@@ -0,0 +1,8 @@
+package ex_07_Java_Operators;
+
+public class Lab035_DoubleEqualtoInterview {
+    public static void main(String[] args) {
+        System.out.println(10==10);
+        System.out.println(10==11);
+    }
+}
