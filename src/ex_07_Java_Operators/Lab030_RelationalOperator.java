d80e4b7d9e176e95ec5641970f97d9fb4e025831
dilip-sdet <dilipraj611@gmail.com>
2026-01-05 13:46:42 +0530

    Commit:I have written Arithmatic program

---
+++ src/ex_07_Java_Operators/Lab030_RelationalOperator.java
@@ -0,0 +1,25 @@
+package ex_07_Java_Operators;
+
+public class Lab030_RelationalOperator {
+    public static void main(String[] args) {
+        /*
+        < -> Less than
+        <= -> Less than or equal to
+        > -> Greater than
+        >= -> Greater than or equal to
+        == -> equal to(checking both values)
+        != -> Not equal
+         */
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a != b);
+
+
+    }
+}
