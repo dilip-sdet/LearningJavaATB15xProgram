8a9148ed8c73f5f2e58ed8e4f4b121341127a32d
dilip-sdet <dilipraj611@gmail.com>
2026-01-05 18:45:12 +0530

    I have written IfElse program

---
+++ src/ex_11_Java_ConditionStatements/Lab062_IfElseInterview.java
@@ -0,0 +1,12 @@
+package ex_11_Java_ConditionStatements;
+
+public class Lab062_IfElseInterview {
+    public static void main(String[] args) {
+        int a = 7;
+        if(a == 10){
+            System.out.println("haha");
+        }else {
+            System.out.println("hoho");
+        }
+    }
+}
