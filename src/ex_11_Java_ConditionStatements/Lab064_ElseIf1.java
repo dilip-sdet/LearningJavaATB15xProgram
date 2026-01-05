4fbc946e7273e2d4050f6c486b04ba2123a26fa5
dilip-sdet <dilipraj611@gmail.com>
2026-01-05 18:58:50 +0530

    I have written IfElse program

--- src/ex_11_Java_ConditionStatements/Lab064_ElseIf1.java
+++ src/ex_11_Java_ConditionStatements/Lab064_ElseIf1.java
@@ -1,8 +1,12 @@
 package ex_11_Java_ConditionStatements;
 
+import java.util.Scanner;
+
 public class Lab064_ElseIf1 {
     public static void main(String[] args) {
-        int age = 32;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int age = sc.nextInt();
         if(age < 18){
             System.out.println(("You are Teenager"));
         } else if (age > 60) {
