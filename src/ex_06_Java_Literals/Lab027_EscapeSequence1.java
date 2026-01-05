1d64f1b53f2a5bcb33c45075e8841de38b05cef6
dilip-sdet <dilipraj611@gmail.com>
2026-01-05 13:09:13 +0530

    Commit:I have written escape sequence Program

---
+++ src/ex_06_Java_Literals/Lab027_EscapeSequence1.java
@@ -0,0 +1,18 @@
+package ex_06_Java_Literals;
+
+public class Lab027_EscapeSequence1 {
+    public static void main(String[] args) {
+         /*
+        \n - next line
+        \t - tab space
+        \b - backspace
+        \r - carriage return
+         */
+        System.out.println(("Dilip\nKumar"));
+        System.out.println(("Dilip\tKumar"));
+        System.out.println(("Dilip \bKumar"));
+        System.out.println(("Dilip\rKumar"));
+    }
+}
+
+
