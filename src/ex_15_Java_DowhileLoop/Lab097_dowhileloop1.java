50a84b903fea0ee56f636101f6cd3d22f3ea04e6
dilip-sdet <dilipraj611@gmail.com>
2026-01-11 05:36:41 +0530

    I have written do while-loop Program

---
+++ src/ex_15_Java_DowhileLoop/Lab097_dowhileloop1.java
@@ -0,0 +1,11 @@
+package ex_15_Java_DowhileLoop;
+
+public class Lab097_dowhileloop1 {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.println((i));
+            i++;
+        }while(i<10);
+    }
+}
---
+++ src/ex_15_Java_DowhileLoop/Lab098_dowhileloop2.java
@@ -0,0 +1,11 @@
+package ex_15_Java_DowhileLoop;
+
+public class Lab098_dowhileloop2 {
+    public static void main(String[] args) {
+        int i=0;
+        do {
+            System.out.println((i));
+            i++;
+        }while(i>10);
+    }
+}
